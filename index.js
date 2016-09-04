21205ddb74728a5a1ed98d695550af543d90d682
Philipp Hug <philipp@hug.cx>
2016-09-04 11:31:05 +0200

    WIP

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// An example for OpenShift platform.
+require('dotenv').config();
+var striptags = require('striptags');
+var request = require('request');
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = process.env.TELEGRAM_BOT_TOKEN;
@@ -7,27 +9,44 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var host = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var domain = process.env.OPENSHIFT_APP_DNS;
 
-var request = require('request');
 var adstatusurl = 'https://mfgt-api.appspot.com/api/v1/aerodromestatus';
 var resurl = 'https://mfgt-api.appspot.com/api/v1/reservations';
 
-console.log("domain: " + domain);
+var polling_options = { polling: true };
+var webhook_options = {webHook: {port: port, host: host}};
 
-var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+var options = domain ? webhook_options : polling_options;
+
+var bot = new TelegramBot(token, options);
 // OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
-bot.setWebHook(domain+':443/bot'+token);
-
+if (domain) {
+  bot.setWebHook(domain+':443/bot'+token);
+}
 
 bot.on('message', function (msg) {
-  if (msg.text == 'status') {
+  var chatId = msg.chat.id;
+console.log(msg.text);
+  if (msg.text == '/status') {
     request({'url': adstatusurl}, function(error, response, body) {
       var status = JSON.parse(body);
-      bot.sendMessage(chatId, status.status);
-      console.log('result');
+      var msg = striptags(status.message);
+      bot.sendMessage(chatId, msg, {'parse_mode': 'HTML'});
       console.log(body);
     });
+  } else if (msg.text == '/webcam') {
+    request({'url': adstatusurl}, function(error, response, body) {
+      var status = JSON.parse(body);
+      console.log(body);
+      request({'url': status.webcam.cams.east.high, 'encoding': null}, function(error, response, east) {
+        request({'url': status.webcam.cams.west.high, 'encoding': null}, function(error, response, west) {
+          bot.sendPhoto(chatId, east);
+          bot.sendPhoto(chatId, west);
+        });
+      });
+    });
+  } else if (msg.text == '/start') {
+    bot.sendMessage(chatId, "Welcome to the LSZT bot");
   } else {
-    var chatId = msg.chat.id;
-    bot.sendMessage(chatId, "Command not found: " + msg);
+    bot.sendMessage(chatId, "Command not found: " + msg.text);
   }
 });
