76742e37493bdd8cb94c5a20a60accc5078b8358
Philipp Hug <philipp@hug.cx>
2016-10-01 21:05:05 +0200

    Add notifier

---
+++ fbnotify.js
@@ -0,0 +1,51 @@
+var firebase = require("firebase");
+var moment = require('moment-timezone');
+
+var FirebaseListener = function (options, cb) {
+  this.options = {
+    firebase: {
+      serviceAccount: "credentials.json",
+      databaseURL: "https://mfgt-flights.firebaseio.com",
+    },
+    tz: "Europe/Zurich",
+    locale: "de",
+  };
+  Object.assign(this.options, options);
+  console.log(this.options);
+  this.cb = cb;
+  this.notify();
+};
+
+FirebaseListener.prototype.notifyMovement = function (val, id) {
+  var dir = id == "departures" ? "dep" : "arr";
+  var msg = dir + " " + moment(val.dateTime).locale(this.options.locale).tz(this.options.tz).format("L LT") + " " + val.immatriculation + " " + val.location + " " + val.lastname;
+  this.cb(msg);
+  console.log(msg);
+}
+
+FirebaseListener.prototype.notifyOnMovement = function (ref, arg) {
+  var self = this;
+  console.log("init");
+  ref.once("value", function (snap) {
+    console.log("vali");
+    var keys = Object.keys(snap.val() || {});
+    var lastIdInSnapshot = keys[keys.length - 1];
+    ref.orderByKey().startAt(lastIdInSnapshot).on("child_added", function (valsnap) {
+      if (valsnap.key === lastIdInSnapshot) {
+        return;
+      }
+      self.notifyMovement(valsnap.val(), arg);
+    });
+  });
+}
+
+FirebaseListener.prototype.notify = function () {
+  firebase.initializeApp(this.options.firebase);
+  var db = firebase.database();
+  var ref = db.ref("departures");
+  this.notifyOnMovement(ref, "departures");
+  ref = db.ref("arrivals");
+  this.notifyOnMovement(ref, "arrivals");
+};
+
+module.exports = FirebaseListener;
--- index.js
+++ index.js
@@ -2,50 +2,78 @@ require('dotenv').config();
 var striptags = require('striptags');
 var request = require('request');
 var TelegramBot = require('node-telegram-bot-api');
+var strftime = require('strftime');
 
 var token = process.env.TELEGRAM_BOT_TOKEN;
 // See https://developers.openshift.com/en/node-js-environment-variables.html
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var host = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var host = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var domain = process.env.OPENSHIFT_APP_DNS;
 
 var adstatusurl = 'https://mfgt-api.appspot.com/api/v1/aerodromestatus';
 var resurl = 'https://mfgt-api.appspot.com/api/v1/reservations';
 
-var polling_options = { polling: true };
-var webhook_options = {webHook: {port: port, host: host}};
+var polling_options = {
+  polling: true
+};
+var webhook_options = {
+  webHook: {
+    port: port,
+    host: host
+  }
+};
 
 var options = domain ? webhook_options : polling_options;
 
 var bot = new TelegramBot(token, options);
 // OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
 if (domain) {
-  bot.setWebHook(domain+':443/bot'+token);
+  bot.setWebHook(domain + ':443/bot' + token);
 }
 
-var help = "Supported commands:\n/status - return aerodrome status\n/webcam - show current webcam images"
+var help = "Supported commands:\n/status - return aerodrome status\n/webcam - show current webcam images";
+
+bot.onText(/\/contact/, function (msg) {
+  var chatId = msg.chat.id;
+  bot.sendContact(chatId, {
+    'phone_number': '+41523663333'
+  });
+});
 
 bot.on('message', function (msg) {
+  console.log(msg.chat);
   var chatId = msg.chat.id;
-  var cmd = msg.text.toLowerCase().trim();
-  cmd = cmd.charAt(0) == '/'? cmd.substring(1) : cmd;
+  var cmd = msg.text ? msg.text.toLowerCase().trim() : "";
+  cmd = cmd.charAt(0) == '/' ? cmd.substring(1) : cmd;
   console.log(msg.text + ':' + cmd);
   if (cmd == 'status') {
     bot.sendChatAction(chatId, 'typing');
-    request({'url': adstatusurl}, function(error, response, body) {
+    request({
+      'url': adstatusurl
+    }, function (error, response, body) {
       var status = JSON.parse(body);
-      var msg = striptags(status.message);
-      bot.sendMessage(chatId, msg, {'parse_mode': 'HTML'});
+      var msg = 'Status: ' + status.status + '\n' + striptags(status.message) + '\n- ' + strftime('%d.%m.%Y %H:%M ', new Date(status.last_update_date)) + ' ' + status.last_update_by;
+      bot.sendMessage(chatId, msg, {
+        'parse_mode': 'HTML'
+      });
       console.log(body);
     });
   } else if (cmd == 'webcam') {
     bot.sendChatAction(chatId, 'upload_photo');
-    request({'url': adstatusurl}, function(error, response, body) {
+    request({
+      'url': adstatusurl
+    }, function (error, response, body) {
       var status = JSON.parse(body);
       console.log(body);
-      request({'url': status.webcam.cams.east.high, 'encoding': null}, function(error, response, east) {
+      request({
+        'url': status.webcam.cams.east.high,
+        'encoding': null
+      }, function (error, response, east) {
         bot.sendPhoto(chatId, east);
-        request({'url': status.webcam.cams.west.high, 'encoding': null}, function(error, response, west) {
+        request({
+          'url': status.webcam.cams.west.high,
+          'encoding': null
+        }, function (error, response, west) {
           bot.sendPhoto(chatId, west);
         });
       });
@@ -58,3 +86,21 @@ bot.on('message', function (msg) {
     bot.sendMessage(chatId, "Command not recognized." + help);
   }
 });
+
+
+function sendChat(msg) {
+  var mfgt_chat_id = -1001071250873;
+  bot.sendMessage(mfgt_chat_id, msg);
+}
+
+var Fbnotify = require("./fbnotify");
+new Fbnotify({
+  firebase: {
+    serviceAccount: {
+      projectId: process.env.FIREBASE_PROJECTID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_CLIENT_PRIVKEY,
+    },
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://mfgt-flights.firebaseio.com",
+  },
+}, sendChat);
